034d09c63c0608c0a8749061f1fb8c8208bcf6ff
Azizxon <muzaffarovazizkhon@gmail.com>
2023-07-10 10:45:36 +0500

    Blog

--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -1,6 +1,6 @@
 <template>
     <h1 class="text-[50px] font-medium text-center mb-10" data-aos="fade-in" data-aos-duration="500" data-aos-delay="500">Blog</h1>
-    <div class="mb-[150px]" v-html="blogStore.one.text"></div>
+    <div class="mb-[150px] container mx-auto border rounded-lg p-4" v-html="blogStore.one.text"></div>
 </template>
 
 <script setup>
