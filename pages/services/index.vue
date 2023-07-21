d9e1c53004fc57f299936677776b1f94ded513a4
Azizxon <muzaffarovazizkhon@gmail.com>
2023-07-22 00:58:09 +0500

    Services

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -8,9 +8,7 @@ const { data } = await useFetch("https://back-portfolio-azizkhon.vercel.app/api/
         <h1 class="text-[50px] font-medium text-center mb-10" data-aos="fade-in" data-aos-duration="500" data-aos-delay="500">Services</h1>
         <div class="all 2xl:gap-[50px] xl:gap-[30px] lg:gap-[20px]"  data-aos="fade-in" data-aos-duration="500" data-aos-delay="750">
 <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow" v-for="(el, ind) in data">
-    <a href="#">
         <img class="rounded-t-lg scale-75" :src="el.image" alt="" />
-    </a>
     <div class="p-5">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ el.service }}</h5>
     </div>
